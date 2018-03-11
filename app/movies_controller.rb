c1f09e85807305db69f30fa4a779f81c2bb47fb7
Travis Clarkson <tk1667@gmail.com>
2018-03-11 23:58:37 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,9 +22,13 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = {__})
+def can_be_created_in_a_block(args = {})
   Movie.create do |m|
-    __
+    m.title = :title
+    m.release_date = :release_date
+    m.director = :director
+    m.lead = :lead
+    m.in_theaters = :in_theaters
   end
 end
 
@@ -52,7 +56,7 @@ def can_find_by_multiple_attributes
 end
 
 def can_find_using_where_clause_and_be_sorted
-  # For this test return all movies released after 2002 and ordered by 
+  # For this test return all movies released after 2002 and ordered by
   # release date descending
   __
 end
@@ -92,11 +96,3 @@ def can_destroy_all_items_at_once
   end
   __
 end
-
-
-
-
-
-
-
-
